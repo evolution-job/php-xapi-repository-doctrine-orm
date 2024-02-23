54db5f2b5bc8579544776eaba2e0ab51dbcc5a15
mathieu.boldo <mathieu.boldo@entrili.com>
2024-02-23 17:04:47 +0100

    introduce StateRepository

---
+++ src/StateRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the xAPI package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace XApi\Repository\ORM;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use XApi\Repository\Doctrine\Mapping\State;
+use XApi\Repository\Doctrine\Repository\Mapping\StateRepository as BaseStateRepository;
+
+/**
+ * @author Mathieu Boldo <mathieu.boldo@entrili.com>
+ */
+final class StateRepository extends EntityRepository implements BaseStateRepository
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function findState(array $criteria)
+    {
+        return $this->findOneBy($criteria);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function storeState(State $state, $flush = true): void
+    {
+        // Store or Update?
+        $mappedState = $this->findState([
+            "stateId"        => $state->stateId,
+            "activityId"     => $state->activityId,
+            "registrationId" => $state->registrationId
+        ]);
+
+        if ($mappedState instanceof State) { // Update
+            $mappedState->data = $state->data;
+            $state = $mappedState;
+        } else {
+            $state->actor = AvoidDuplicatesHelper::findActor($this->_em->createQueryBuilder(), $state->actor);
+        }
+
+        try {
+            $this->_em->persist($state);
+
+            if ($flush) {
+                $this->_em->flush();
+            }
+        } catch (OptimisticLockException|ORMException $e) {
+            //..
+        }
+    }
+}
